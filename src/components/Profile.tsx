987471ac49f4a737f75827f3182b69eca3a190da
rickutino <rickutino@gmail.com>
2021-02-23 22:33:03 +0900

    add profile component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import { Profiler } from "inspector";
+
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/rickutino.png" alt="Ricardo Utino"/>
+      <div>
+        <strong>Ricardo Utino</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  )
+}
