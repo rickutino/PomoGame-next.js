73859253fc27f6699110c601ad8d4d0342221283
Ricardo Utino <rickutino@gmail.com>
2021-02-24 03:13:12 +0000

    add icon level in profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
       <img src="https://github.com/rickutino.png" alt="Ricardo Utino"/>
       <div>
         <strong>Ricardo Utino</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+        </p>
       </div>
     </div>
   )
