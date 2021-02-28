1795fafd36f977680cfc2bad27282f739a172a61
rickutino <rickutino@gmail.com>
2021-02-28 13:24:04 +0900

    add challenge.json in ChallengeContext

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,16 @@
-import { create } from 'domain';
 import { createContext, ReactNode, useState } from 'react';
+import challenges from '../../challenges.json';
 
-
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number
+}
 interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
 }
@@ -20,13 +25,17 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
   function levelUp() {
     setLevel(level + 1);
   }
 
   function startNewChallenge() {
-    console.log('New Challenge');
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
   }
 
   return(
@@ -35,8 +44,9 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         level,
         currentExperience,
         challengesCompleted,
+        activeChallenge,
         levelUp,
-        startNewChallenge
+        startNewChallenge,
       }}
     >
       {children}
